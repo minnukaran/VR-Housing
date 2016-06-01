e2cc07b21195f28215d830e556b37ff95228d4b9
sohamkamani <sohamkamani@gmail.com>
2016-06-01 14:42:56 +0530

    boxes are getting added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
+require('aframe')
 
-document.write('My App...');
-console.log('Working...');
 
+let i = 0
+
+setInterval(() => {
+	const scene = document.getElementById('my-scene')
+
+	const box = document.createElement('a-box')
+	box.setAttribute('position', '-1 0.5 ' + i)
+	box.setAttribute('width', '1')
+	box.setAttribute('height', '1')
+	box.setAttribute('depth', '1')
+	box.setAttribute('color', 'tomato')
+	i += 1
+	scene.appendChild(box)
+}, 2000)
